ca3c8af45d597ba45f08200a17260a8ff59e9300
Tiko <tiko@Tikos-MacBook-Air.local>
2023-04-15 20:12:32 +0400

    asd

--- next.config.js
+++ next.config.js
@@ -1,13 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    compress: true,
-    images: {
-        unoptimized: true
-    }
-}
+// /** @type {import('next').NextConfig} */
+// const withVideos = require('next-videos')
+//
+// const nextConfig = {
+//     reactStrictMode: true,
+//     compress: true,
+//     images: {
+//         unoptimized: false
+//     }
+// }
+//
+//
+// module.exports = nextConfig
+// module.exports = withVideos()
 
+
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 const withVideos = require('next-videos')
 
-module.exports = nextConfig
-module.exports = withVideos()
+module.exports = withPlugins([
+    withVideos
+
+    // your other plugins here
+
+], {
+    reactStrictMode: true,
+    images: {
+        disableStaticImages: true,
+        unoptimized: true
+    },
+});
